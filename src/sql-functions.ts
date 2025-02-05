a1bda46f6595354f1106fc89ebf9378fb5c86644
Baptiste Pillot <baptiste@pillot.fr>
2025-02-05 18:00:19 +0100

    Correct operators for greater and greaterOrEqual functions

--- src/sql-functions.ts
+++ src/sql-functions.ts
@@ -5,8 +5,8 @@ export default class Function
 }
 
 export const equal          = (value: any) => new Function(equal.name,          ' = ?',    value)
-export const greater        = (value: any) => new Function(greater.name,        ' < ?',    value)
-export const greaterOrEqual = (value: any) => new Function(greaterOrEqual.name, ' <= ?',   value)
+export const greater        = (value: any) => new Function(greater.name,        ' > ?',    value)
+export const greaterOrEqual = (value: any) => new Function(greaterOrEqual.name, ' >= ?',   value)
 export const less           = (value: any) => new Function(less.name,           ' < ?',    value)
 export const lessOrEqual    = (value: any) => new Function(lessOrEqual.name,    ' <= ?',   value)
 export const like           = (value: any) => new Function(like.name,           ' LIKE ?', value)
